4269db9b2d87005c7ff74348736ed81a529fb7c6
Andris Reinman <andris@kreata.ee>
2014-06-29 01:39:04 +0300

    Recorded current status of development

---
+++ src/mailsender.js
@@ -0,0 +1,213 @@
+'use strict';
+
+var BuildMail = require('buildmail');
+var libmime = require('libmime');
+var stream = require('stream');
+var fs = require('fs');
+
+function MailSender(mail, plugins) {
+    this.mail = mail || {};
+    this.plugins = plugins || {};
+    this.message = {};
+}
+
+MailSender.prototype.compose = function() {
+    this.message.attachments = this.getAttachments();
+    this.message.htmlNode = this.message.alternatives.filter(function(alternative) {
+        return /^text\/html\b/i.test(alternative.contentType);
+    }).pop();
+    this.message.alternatives = this.getAlternatives(!!this.message.htmlNode);
+
+    this.message.useRelated = !!(this.message.htmlNode && this.message.attachments.related.length);
+    this.message.useAlternative = this.message.alternatives.length > 1;
+    this.message.useMixed = this.message.alternatives.length + this.message.attachments.attached.length > 1;
+
+    if (this.message.useMixed) {
+        this.message.builder = this.createMixed();
+    } else if (this.message.useAlternative) {
+        this.message.builder = this.createAlternative();
+    } else if (this.message.useRelated) {
+        this.message.builder = this.createRelated();
+    } else {
+        // FIXME: add remaining nodes. Should be only one
+    }
+};
+
+MailSender.prototype.createMixed = function(parentNode) {
+    var node;
+
+    if (!parentNode) {
+        node = new BuildMail('multipart/mixed');
+    } else {
+        node = parentNode.createChild('multipart/mixed');
+    }
+
+    if (this.message.useAlternative) {
+        this.createAlternative(node);
+    } else if (this.message.useRelated) {
+        this.createRelated(node);
+    }
+
+    // FIXME: add other nodes
+
+    return node;
+};
+
+MailSender.prototype.createAlternative = function(parentNode) {
+    var node;
+
+    if (!parentNode) {
+        node = new BuildMail('multipart/alternative');
+    } else {
+        node = parentNode.createChild('multipart/alternative');
+    }
+
+    this.message.alternatives.forEach(function(alternative) {
+        var child;
+
+        if (this.message.useRelated && this.message.htmlNode === alternative) {
+            this.createRelated(node);
+        } else {
+            child = node.createChild(alternative.contentType, {
+                filename: alternative.filename
+            });
+
+            child.setContent(alternative.contents);
+        }
+    }.bind(this));
+
+    return node;
+};
+
+MailSender.prototype.createRelated = function(parentNode) {
+    var node;
+
+    if (!parentNode) {
+        node = new BuildMail('multipart/related; type="text/html"');
+    } else {
+        node = parentNode.createChild('multipart/related; type="text/html"');
+    }
+
+    node.createChild(this.message.htmlNode.contentType).setContent(this.message.htmlNode.contents);
+
+    this.message.attachments.related.forEach(function(alternative) {
+        var child;
+
+        child = node.createChild(alternative.contentType, {
+            filename: alternative.filename
+        });
+
+        if (alternative.cid) {
+            child.setHeader('Content-Id', '<' + alternative.cid.replace(/[<>]/g, '') + '>');
+        }
+
+        child.setContent(alternative.contents);
+    }.bind(this));
+};
+
+
+MailSender.prototype.getAttachments = function(findRelated) {
+    var attachments = [].concat(this.mail.attachments || []).map(function(attachment) {
+        var data = {
+            contentType: attachment.contentType ||  
+                libmime.detectMimeType(attachment.filename || attachment.filePath || attachment.href || 'bin')
+        };
+
+        if (attachment.filename) {
+            data.filename = attachment.filename;
+        }
+
+        if (/^https?:\/\//i.test(attachment.filePath)) {
+            attachment.href = attachment.filePath;
+            attachment.filePath = undefined;
+        }
+
+        if (attachment.cid) {
+            data.cid = attachment.cid;
+        }
+
+        if (attachment.filePath) {
+            data.contents = {
+                path: attachment.filePath
+            };
+        } else if (attachment.href) {
+            data.contents = {
+                href: attachment.href
+            };
+        } else {
+            data.contents = data.contents || '';
+        }
+
+        return data;
+    }.bind(this));
+
+    if (!findRelated) {
+        return {
+            attached: attachments,
+            related: []
+        };
+    } else {
+        return {
+            attached: attachments.filter(function(attachment) {
+                return !attachment.cid;
+            }),
+            related: attachments.filter(function(attachment) {
+                return !!attachment.cid;
+            })
+        };
+    }
+};
+
+MailSender.prototype.getAlternatives = function() {
+    var alternatives = [];
+
+    if (this.mail.html) {
+        alternatives.push({
+            contentType: 'text/html; charset=utf-8',
+            contents: this.mail.html
+        });
+    }
+
+    if (this.mail.text) {
+        alternatives.push({
+            contentType: 'text/plain; charset=utf-8',
+            contents: this.mail.text
+        });
+    }
+
+    [].concat(this.mail.alternatives || []).forEach(function(alternative) {
+        var data = {
+            contentType: alternative.contentType ||  
+                libmime.detectMimeType(alternative.filename || alternative.filePath || alternative.href || 'txt')
+        };
+
+        if (alternative.filename) {
+            data.filename = alternative.filename;
+        }
+
+        if (/^https?:\/\//i.test(alternative.filePath)) {
+            alternative.href = alternative.filePath;
+            alternative.filePath = undefined;
+        }
+
+        if (alternative.filePath) {
+            data.contents = {
+                path: alternative.filePath
+            };
+        } else if (alternative.href) {
+            data.contents = {
+                href: alternative.href
+            };
+        } else {
+            data.contents = data.contents || '';
+        }
+
+        alternatives.push(data);
+    }.bind(this));
+
+    return alternatives;
+};
+
+MailSender.prototype.send = function(transporter, callback) {
+    transporter.send(this.message.getEnvelope(), this.message.createReadStream(), callback);
+};
--- src/nodemailer.js
+++ src/nodemailer.js
@@ -1,6 +1,32 @@
 'use strict';
 
+var MailSender = require('./mailsender');
+
 // Export createTransport method
 module.exports.createTransport = function(transporter) {
-    return transporter;
+    return new Nodemailer(transporter);
+};
+
+function Nodemailer(transporter) {
+    this.transporter = transporter;
+
+    this.plugins = {
+        html: [],
+        text: [],
+        message: []
+    };
+}
+
+Nodemailer.prototype.use = function(plugin) {
+    Object.keys(plugin || {}).forEach(function(key){
+        if(key in this.plugins){
+            this.plugins.push(plugin[key]);
+        }
+    }.bind(this));
+};
+
+Nodemailer.prototype.sendMail = function(mail, callback) {
+    var sender = new MailSender(mail, this.plugins);
+    sender.compose();
+    sender.send(this.transporter, callback);
 };
